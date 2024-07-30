b2c7f6476d5b386a500bd9c2348cdb7f26e6c271
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-07-31 07:58:53 +0900

    Delete Chapter 03. 주석으로 코드를 명확히 하기 directory

--- Chapter 03. 주석으로 코드를 명확히 하기/예제/2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-main()
-{
-    float a, b; printf("보너스를 얼마나 받았습니까?"); scanf(" %f",
-        &a); b = .85 * a; printf("만약 15%를 자선단체에 기부하더라도 당신은 아직 %.2f를 가지고 있습니다.", b); return 0;
-}
