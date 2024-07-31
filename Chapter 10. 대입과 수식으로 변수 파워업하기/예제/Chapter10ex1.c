15123a5edb1c6e52320482ee776abe1288ac8e11
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-07-31 18:22:15 +0900

    Create Chapter10ex1.c

---
+++ Chapter 10. 대입과 수식으로 변수 파워업하기/예제/Chapter10ex1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+main()
+{
+    int ctr = 0;
+
+    ctr = ctr + 1; // 카운터를 1로 증가한다.
+    printf("카운터의 값은 %d.\n", ctr);
+    ctr = ctr + 1; // 카운터를 2로 증가한다.
+    printf("카운터의 값은 %d.\n", ctr);
+    ctr = ctr + 1; // 카운터를 3로 증가한다.
+    printf("카운터의 값은 %d.\n", ctr);
+    ctr = ctr + 1; // 카운터를 4로 증가한다.
+    printf("카운터의 값은 %d.\n", ctr);
+    ctr = ctr + 1; // 카운터를 5로 증가한다.
+    printf("카운터의 값은 %d.\n", ctr);
+    ctr = ctr - 1; // 카운터를 4로 증가한다.
+    printf("카운터의 값은 %d.\n", ctr);
+    ctr = ctr - 1; // 카운터를 3로 증가한다.
+    printf("카운터의 값은 %d.\n", ctr);
+    ctr = ctr - 1; // 카운터를 2로 증가한다.
+    printf("카운터의 값은 %d.\n", ctr);
+    ctr = ctr - 1; // 카운터를 1로 증가한다.
+    printf("카운터의 값은 %d.\n", ctr);
+
+    return 0;
+}
