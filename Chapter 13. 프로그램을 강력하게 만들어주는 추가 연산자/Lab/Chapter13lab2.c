331643cce393e2f9f78146ea19c6312f2a437cd2
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-08-01 06:58:33 +0900

    Create Chapter13lab2.c

---
+++ Chapter 13. 프로그램을 강력하게 만들어주는 추가 연산자/Lab/Chapter13lab2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main()
+{
+    int x = 8, y = 10;
+
+    printf("x=%d\n", x);
+    printf("x=%d\n", ++x);
+    printf("x=%d\n", x);
+
+    printf("y=%d\n", y);
+    printf("y=%d\n", y++);
+    printf("y=%d\n", y);
+    return 0;
+}
